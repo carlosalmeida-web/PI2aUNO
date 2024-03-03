25cf7cac282063ac74785a6bde5a205d04920845
Jaih4 <61789288+Jaih4@users.noreply.github.com>
2024-03-03 00:41:43 -0300

    Create Uno.c
    listas ligadas

---
+++ Uno.c
@@ -0,0 +1,23 @@
+typedef struct manilha
+{
+    int numero;
+    char categoria;
+    int *prox, *ant;
+}manilha;
+
+typedef struct baralho
+{
+    int numero;
+    char categoria;
+    int *prox;
+}baralho;
+
+void embaralhar(int *cartas){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
